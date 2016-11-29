817c9c498a4a992f2c19d62da97378e787b47331
Jaysig <jasontsigmon@gmail.com>
2016-11-29 16:22:52 -0600

    initial version of user schema

--- models/user.js
+++ models/user.js
@@ -3,11 +3,12 @@ const Schema = mongoose.Schema;
 
 // Define our model
 const userSchema = new Schema({
-  email: { type: String unique: true },
+  email: { type: String unique: true, lowercase: true },
   password: String
 });
 
 // Create the model class
-
+const ModelClass = mongoose.model('user', userSchema);
 
 // Export the model
+module.exports = ModelClass;
